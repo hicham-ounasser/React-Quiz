7ba2b0163bc51d26966d24c137a07fa3af415c43
Hicham Ounasser <ounasser.hicham@gmail.com>
2025-08-15 16:01:41 +0100

    finilising the quiz

--- src/component/App.js
+++ src/component/App.js
@@ -6,6 +6,8 @@ import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./NextButton";
+import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
 
 const initialState = {
   question: [],
@@ -13,6 +15,7 @@ const initialState = {
   index: 0,
   answer: null,
   points: 0,
+  highscore: 0,
 };
 
 function reducer(state, action) {
@@ -42,18 +45,27 @@ function reducer(state, action) {
       };
     case "nextQuestion":
       return { ...state, index: state.index + 1, answer: null };
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
     default:
       throw new Error("Action unknown");
   }
 }
 
 export default function App() {
-  const [{ question, status, index, answer }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
+  const [{ question, status, index, answer, points, highscore }, dispatch] =
+    useReducer(reducer, initialState);
 
   const numQuestions = question.length;
+  const maxPossiblePoints = question.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
@@ -73,13 +85,32 @@ export default function App() {
         )}
         {status === "active" && (
           <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
             <Question
               question={question[index]}
               dispatch={dispatch}
               answer={answer}
             />
-            <NextButton dispatch={dispatch} answer={answer} />
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+              index={index}
+              numQuestions={numQuestions}
+            />
           </>
+        )}
+        {status === "finished" && (
+          <FinishScreen
+            points={points}
+            maxPossiblePoints={maxPossiblePoints}
+            highscore={highscore}
+          />
         )}
       </Main>
     </div>
---
+++ src/component/FinishScreen.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const FinishScreen = ({ points, maxPossiblePoints, highscore }) => {
+  const percentage = (points / maxPossiblePoints) * 100;
+
+  let emoji;
+  if (percentage === 100) emoji = "🏆✨";
+  if (percentage >= 80 && percentage < 100) emoji = "💪✅";
+  if (percentage >= 50 && percentage < 80) emoji = "👍🙂";
+  if (percentage >= 0 && percentage < 50) emoji = "⚠️😕";
+  if (percentage === 0) emoji = "❌😞";
+
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span> You scored <strong>{points}</strong> out of{" "}
+        {maxPossiblePoints} ({Math.ceil(percentage)}%)
+      </p>
+
+      <p className="highscore">(Highscore: {highscore} points)</p>
+    </>
+  );
+};
+
+export default FinishScreen;
